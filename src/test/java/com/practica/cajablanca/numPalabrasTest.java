7eb36946aaf1b63dd458f160974da4900c1b78b0
Mega646 <jsanz.r64@gmail.com>
2021-11-25 17:37:51 +0100

    leerFicheroTest creado

--- src/test/java/com/practica/cajablanca/numPalabrasTest.java
+++ src/test/java/com/practica/cajablanca/numPalabrasTest.java
@@ -5,8 +5,21 @@ package com.practica.cajablanca;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import com.cajanegra.AbstractSingleLinkedListImpl;
+import com.cajanegra.EmptyCollectionException;
+import com.cajanegra.SingleLinkedListImpl;
 
 
 
@@ -16,6 +29,12 @@ public class numPalabrasTest {
 	
 	@BeforeAll
 	public static void init()
+	{
+		editor = new Editor();
+	}
+	
+	@BeforeEach
+	public void update()
 	{
 		editor = new Editor();
 	}
@@ -34,7 +53,6 @@ public class numPalabrasTest {
 	public void numPalabrasTestPath2()
 	{
 		editor.leerFichero("path2.txt");
-		System.out.println(editor.size());
 		assertThrows(IllegalArgumentException.class, () -> {
 			editor.numPalabras(1, 3, null);
 		});
@@ -54,5 +72,56 @@ public class numPalabrasTest {
 	{
 		editor.leerFichero("path4.txt");
 		assertEquals(0, editor.numPalabras(5, 3, null));
+	}
+	
+	@Test
+	public void leerFicheroTest()
+	{
+		File file = new File("leerFicheroPrueba.txt");
+		String[] testLine1 = {"Lorem", "ipsum", "dolor"};
+		String[] testLine2 = {"sit", "amet", "consectetur"};
+		String[] testLine3 = {"adipisicing", "elit"};
+		String[][] tests = {testLine1, testLine2, testLine3};
+		String test = "Lorem ipsum dolor\r\nsit amet consectetur\r\nadipisicing elit";
+		FileWriter fw = null;
+		BufferedWriter bw = null;
+		try
+		{
+			fw = new FileWriter(file);
+			bw = new BufferedWriter(fw);
+			bw.write(test);
+		}
+		catch(IOException e)
+		{
+			e.printStackTrace();
+		}
+		finally
+		{
+			try
+			{
+				bw.close();
+			}
+			catch (IOException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		editor.leerFichero("leerFicheroPrueba.txt");
+		for(int i = 1; i < 4; i++)
+		{
+			try
+			{
+				SingleLinkedListImpl<String> fichText = (SingleLinkedListImpl<String>) editor.getLinea(i);
+				SingleLinkedListImpl<String> textTest = new SingleLinkedListImpl<String>(tests[i-1]);
+				for(int j = 1; j <= fichText.size(); j++)
+				{
+					assertEquals(fichText.getAtPos(j), textTest.getAtPos(j));
+				}
+			}
+			catch(EmptyCollectionException e)
+			{
+				e.printStackTrace();
+			}
+		}
 	}
 }
